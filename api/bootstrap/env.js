1d28569e5b79cf66905cbb8b5ba252caba2de908
Erwan Gilbert <gilberterwan@gmail.com>
2019-02-06 09:53:19 +0100

    marvel api testing

--- api/bootstrap/env.js
+++ api/bootstrap/env.js
@@ -1,7 +1,7 @@
 module.exports.MARVEL_API_ROOT = 'http://gateway.marvel.com/v1/public';
 
-module.exports.MARVEL_PRIVATE_KEY = 'b7d68c143ead607d4509af648548b99e46df0320';
+module.exports.MARVEL_PRIVATE_KEY = '';
 
-module.exports.MARVEL_PUBLIC_KEY = '85fd19ac3137eaf235b83123614d0f31';
+module.exports.MARVEL_PUBLIC_KEY = '';
 
 
